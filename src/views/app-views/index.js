2425ae168f891f68471223c44fe5220c25cc0b7f
Vineet Yadav <vineetyadav8588@gmail.com>
2020-09-22 14:40:10 +0530

    url fixing

--- src/views/app-views/index.js
+++ src/views/app-views/index.js
@@ -6,6 +6,7 @@ export const AppViews = ({match}) => {
   return (
     <Suspense fallback={<Loading cover="content"/>}>
       <Switch>
+        <Route path={`${match.url}/dashboard`} component={lazy(() => import(`./dashboard`))} />
         <Route path={`${match.url}/dashboards`} component={lazy(() => import(`./dashboards`))} />
         <Route path={`${match.url}/apps`} component={lazy(() => import(`./apps`))} />
         <Route path={`${match.url}/components`} component={lazy(() => import(`./components`))} />
@@ -13,7 +14,7 @@ export const AppViews = ({match}) => {
         <Route path={`${match.url}/maps`} component={lazy(() => import(`./maps`))} />
         <Route path={`${match.url}/charts`} component={lazy(() => import(`./charts`))} />
         <Route path={`${match.url}/docs`} component={lazy(() => import(`./docs`))} />
-        <Redirect from={`${match.url}`} to={`${match.url}/dashboards`} />
+        <Redirect from={`${match.url}`} to={`${match.url}/dashboard/inbox`} />
       </Switch>
     </Suspense>
   )
